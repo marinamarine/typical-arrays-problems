ae3cf38b248f980563f29c3a32388aacea6da372
marinamarine <marinakarpovskaya@gmail.com>
2020-03-20 01:12:49 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0;
+  }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0;
+  }
+  return array.reduce((result, current) => result + current) / array.length;
 }
